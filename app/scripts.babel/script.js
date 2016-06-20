2ae630edee3e1a4e8572c3ec0e3cfaf633a76d1b
Matheus <matheus.frndes@gmail.com>
2016-06-20 01:35:52 -0300

    Automatically shows the "DuoBot's translation"

--- app/scripts.babel/script.js
+++ app/scripts.babel/script.js
@@ -1,1 +1,24 @@
-// TODO
+/**
+ * Created by matheus on 6/19/16.
+ */
+'use strict';
+
+const observer = new MutationObserver(() => {
+  const showBotTranslation = $('.toggle-machine-translation:visible');
+
+  if (showBotTranslation.length) {
+    if (!$(showBotTranslation).hasClass('active')) {
+      $(showBotTranslation).click(); // automatically shows the "DuoBot's translation"
+    }
+  }
+});
+
+var observerConfig = {
+  attributes: true,
+  childList: true,
+  characterData: true,
+  subtree: true
+};
+
+//noinspection JSCheckFunctionSignatures
+observer.observe(document.body, observerConfig);
